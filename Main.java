bc7d75559ea401dc50806651b7f4f9b0226c941a
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-03-09 13:23:06 +0000

    Finished 4.11!

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
       experimentData= new int[10][3]; //10 rows 3 columns 
       
       //INITIALIZE a 2D array (declared + create)
-      String[][] seatingChart = new String[4][6] // total number of items 4*6= 24
+      String[][] seatingChart = new String[4][6]; // total number of items 4*6= 24
 
       //SET A VALUE by accessing a cell
       //ACCESS a cell: arrayName[row][col]
@@ -23,7 +23,7 @@ public class Main {
       seatingChart[0][5] = "Sohaila"; //6 columns, 5 is the FINAL INDEX! 
       seatingChart[1][0] = "Ethan";
       seatingChart[1][5] = "Zach";
-      seatinghChart[2][1] = "Aaniyah"
+      seatingChart[2][1] = "Aaniyah";
       seatingChart[2][3] = "Simeon";
       seatingChart[2][4] = "Bowe"; //second to last column 
       seatingChart[3][0] = "Max S";
